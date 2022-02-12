b7f117c74062d1c6b2b9009c3112252929b0873c
noctispine <pinefromw@gmail.com>
2022-02-12 13:14:45 +0300

    formatted

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -1,18 +1,15 @@
-import { useState } from "react";
-import { projects } from "../data";
-import Project from "./Project";
-import { motion } from "framer-motion";
-import variants from "../styles/animation";
+import { useState } from 'react'
+import { projects } from '../data'
+import Project from './Project'
+import { motion } from 'framer-motion'
+import variants from '../styles/animation'
 
 const ProjectSection = () => {
-  const [showDetail, setShowDetail] = useState<number | null>(null);
+  const [showDetail, setShowDetail] = useState<number | null>(null)
 
-  
   return (
-    <div style={{ marginTop: "-1rem" }}>
-      <div
-        className="relative grid grid-cols-12 gap-4 my-3"
-      >
+    <div style={{ marginTop: '-1rem' }}>
+      <div className="relative grid grid-cols-12 gap-4 my-3">
         {projects.map((project) => (
           <motion.div
             variants={variants.fadeInUpmost}
@@ -21,12 +18,17 @@ const ProjectSection = () => {
             key={project.id}
             className="col-span-12 p-2 md:col-span-6 xl:col-span-4"
           >
-            <Project data={project} showDetail={showDetail} setShowDetail={setShowDetail} id={project.id}/>
+            <Project
+              data={project}
+              showDetail={showDetail}
+              setShowDetail={setShowDetail}
+              id={project.id}
+            />
           </motion.div>
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ProjectSection;
+export default ProjectSection
