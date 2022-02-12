da85dec7e2ebaad9abc9b10f6df9d9584a66677d
noctispine <pinefromw@gmail.com>
2022-02-12 13:09:21 +0300

    add book type

--- type.ts
+++ type.ts
@@ -1,24 +1,31 @@
-import { IconType } from "react-icons";
+import { IconType } from 'react-icons'
 
 export interface SkillInterface {
-  name: string;
-  Icon: IconType;
+  name: string
+  Icon: IconType
 }
 
 export interface AboutInterface {
-  Icon: IconType;
-  title: string;
-  body: string;
+  Icon: IconType
+  title: string
+  body: string
 }
 
 export interface ProjectInterface {
-  id: number;
-  name: string;
-  description: string;
-  image_url: string;
-  live_url: string;
-  github_url: string;
-  tags: string[];
+  id: number
+  name: string
+  description: string
+  image_url: string
+  live_url: string
+  github_url: string
+  tags: string[]
 }
 
-type Stack = "react" | "next.js" | "typescript" | "sass/scss" | "tailwind";
+export interface BookInterface {
+  title: string
+  author: string[]
+  image: string
+  url: string
+}
+
+type Stack = 'react' | 'next.js' | 'typescript' | 'sass/scss' | 'tailwind'
